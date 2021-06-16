95668305362e201d64312c24488bf5544ebdadf6
cyclops77 <alfian000000@gmail.com>
2021-06-16 14:52:03 +0700

    as

--- lib/core/server.dart
+++ lib/core/server.dart
@@ -1,5 +1,5 @@
 class Server {
-  static String endPoint = "http://192.168.1.12:10000/api/v1/";
+  static String endPoint = "http://192.168.1.9:10000/api/v1/";
   static String storageEndPoint = endPoint + "storage/";
 
   static String now = endPoint;
